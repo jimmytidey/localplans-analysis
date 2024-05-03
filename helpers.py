26908ba24b8343e0ff5e0ae33ba7a4abd510dd04
Jimmy Tidey <jimmytidey@TPXi346.cust.communityfibre.co.uk>
2024-05-03 23:24:30 +0100

    added 'unstructured' for chunking

--- helpers.py
+++ helpers.py
@@ -1,11 +1,115 @@
 
 import PyPDF2
-import os, json,re,hashlib
+import os, json,re,hashlib, pprint
 import psycopg2
 from psycopg2 import extras
 from nltk.tokenize import sent_tokenize
 from langchain.embeddings import SentenceTransformerEmbeddings
 import pprint
+
+from unstructured.partition.pdf import partition_pdf
+from unstructured.chunking.title import chunk_by_title
+from unstructured.cleaners.core import clean
+import nltk
+import ssl,re
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('punkt')    
+
+def pdf_to_chunks(file_path): 
+    elements = partition_pdf(filename=file_path, strategy='fast', url=None)
+    
+    chunks = chunk_by_title(elements, max_characters=1000)
+    total_word_count = 0 
+    for chunk in chunks:
+        words = chunk.text.split()
+        word_count = len(words) 
+        total_word_count += word_count
+    print("total wordcount after chunking " + str(total_word_count))    
+
+    print(chunks[5].text)
+    return chunks 
+    
+
+def embed_send_to_db(chunks, filename, metadata):
+   
+    print('inserting text fragments into db...')
+
+    #Batch 
+    batch_size = 10
+    chunk_batches = [chunks[x:x+batch_size] for x in range(0, len(chunks), batch_size)]
+
+    pg = Postgres()
+    embedding_model = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+
+    for chunk_batch in chunk_batches:
+        text_fragments = []
+        for chunk in chunk_batch:
+            clean_text = clean(chunk.text,extra_whitespace=True, dashes=True,bullets=True,trailing_punctuation=True)
+            vector = embedding_model.embed_documents(clean_text)
+            hash = hashlib.sha256(clean_text.encode()).hexdigest()
+            text_fragment = (clean_text, clean_text, metadata, vector[0], filename, hash)
+            text_fragments.append(text_fragment)
+    
+            pprint.pp((clean_text,metadata, vector[0][0:3], filename, hash))
+
+        pg.insert_text_fragments(text_fragments)
+
+class Postgres:
+
+    def __init__(self):
+        try:
+            DATABASE_URL = os.environ["DATABASE_URL"]
+            self.conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+
+        except psycopg2.Error as e:
+            print("Error:", e)
+
+    def query(self, query):
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(query)
+            result = cursor.fetchall()
+            cursor.close()
+            self.conn.close()
+            return result
+
+        except psycopg2.Error as e:
+            print("Error:", e)
+
+    def insert_text_fragments(self, batch):
+
+        try:
+            cursor = self.conn.cursor()
+            insert_query = "INSERT INTO text_fragments (plain_text, full_text, metadata, vector, filename, hash) VALUES (%s, to_tsvector('english', %s), %s, %s, %s, %s)"     
+            #interpolated_query = cursor.mogrify(insert_query, (text, text, json.dumps(metadata), vector, filename, hash))
+            #print("Interpolated SQL query:", interpolated_query.decode("utf-8"))  
+            extras.execute_batch(cursor, insert_query, batch)
+            self.conn.commit()
+            cursor.close()
+
+        except psycopg2.Error as e:
+            print("Error:", e)
+
+    def close(self):
+        self.conn.close()
+
+
+def list_files(path):
+    # Get the list of entries in the directory
+    entries = os.listdir(path)
+
+    # Filter out only files
+    files = [entry for entry in entries if os.path.isfile(os.path.join(path, entry))]
+
+    return sorted(files) 
 
 def pdf_to_text(source_pdf_folder_path, file_name):
 
@@ -35,8 +139,9 @@ def pdf_to_text(source_pdf_folder_path, file_name):
         print(file_name)
         print(e)
 
-def chunk_embed_send_to_db(text, filename, metadata):
-   
+
+
+def plain_text_chunker():
 
     # Split text
     split_texts_list = sent_tokenize(text)
@@ -59,75 +164,4 @@ def chunk_embed_send_to_db(text, filename, metadata):
     print('number of triplets ', len(triplets)) 
     # Truncate triplets to 1000 characters
     triplets_trucated = [i[:1000] for i in triplets][0:100]
-
-    print('number of triplets truncated', len(triplets_trucated)) 
-    pprint.pprint(triplets_trucated)
-    # Insert into Postgres
-    postgres_connection = Postgres()
-    embedding_model = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-    print('inserting text fragments into db...')
-
-    batch_size = 10
-    text_batches = [triplets_trucated[x:x+batch_size] for x in range(0, len(triplets_trucated), batch_size)]
-   
-    print("number of batches", len(text_batches))
-    print("First batch length", len(text_batches[0]))
-
-    for text_batch in text_batches:
-        print("Text batch", text_batch)
-        print("Text batch length", len(text_batch))
-    
-    for text_batch in text_batches[5]:
-        text_fragments = []
-        for text in text_batch[5]:
-            vector = embedding_model.embed_documents(text)
-            hash = hashlib.sha256(text.encode()).hexdigest()
-            text_fragments.append((text, text, metadata, vector, filename, hash))
-            print("Text fragment batch", text_fragments[0:5])
-            print('...')
-            print("Text fragment batch", text_fragments[-5:])
-
-        #postgres_connection.insert_text_fragments(text_fragments)
-
-
-class Postgres:
-
-    def __init__(self):
-        try:
-            DATABASE_URL = os.environ["DATABASE_URL"]
-            self.conn = psycopg2.connect(DATABASE_URL, sslmode="require")
-
-        except psycopg2.Error as e:
-            print("Error:", e)
-
-    def query(self, query):
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute(query)
-            result = cursor.fetchall()
-            cursor.close()
-            self.conn.close()
-            return result
-
-        except psycopg2.Error as e:
-            print("Error:", e)
-
-    def insert_text_fragments(self, batch):
-
-        try:
-            cursor = self.conn.cursor()
-            print("Inserting text fragment", batch[0:10])           
-            #interpolated_query = cursor.mogrify(insert_query, (text, text, json.dumps(metadata), vector[0], filename, hash))
-            #print("Interpolated SQL query:", interpolated_query.decode("utf-8"))  
-            cursor.execute(insert_query, (text, text,json.dumps(metadata),vector[0],filename, hash))
-            extras.execute_batch(cursor, insert_query, batch)
-            self.conn.commit()
-            cursor.close()
-
-        except psycopg2.Error as e:
-            print("Error:", e)
-
-    def close(self):
-        self.conn.close()
-
 
